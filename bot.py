6216687488b8fc2c3bc7721e9b2b0a6adbcdf110
devilla <dev.koold@gmail.com>
2026-01-14 20:08:30 +0530

    reddit-bot

---
+++ bot.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+import time
+
+URL = "https://www.reddit.com/r/Pokemonexchange/new.json"
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/120.0.0.0 Safari/537.36"
+}
+
+last_seen_id = None
+while True:
+    response = requests.get(URL, headers=HEADERS)
+    data = response.json()
+    if 'data' in data and 'children' in data['data'] and len(data['data']['children']) > 0:
+        post = data['data']['children'][0]['data']
+        if post.get('id') != last_seen_id:
+            print("Title:", post.get('title'))
+            print("Author:", post.get('author'))
+            print("Link: https://reddit.com" + post.get('permalink'))
+            print("Content:", post.get('selftext')[:200])
+            print()
+            last_seen_id = post.get('id')
+        else:
+            print("No new posts.")
+    else:
+        print("No recent posts found.")
+    time.sleep(60)
